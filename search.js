4415d2158151eb3466d9f69a921917d1abbdfcb9
hyoddi <studybeen22@gmail.com>
2025-07-26 21:31:10 +0900

    searchSizeHadle

---
+++ search.js
@@ -0,0 +1,22 @@
+const searchForm = document.querySelector('.search-form');
+const searchInput = document.querySelector('.search-input');
+const searchButton = document.querySelector('.search-button');
+
+
+function handleSearchInputSize(){
+    if (window.innerWidth < 800) {
+        searchInput.style.width = '300px';
+    } else if (window.innerWidth < 1000) {
+        searchInput.style.width = window.innerWidth - 500 + 'px';
+    }   else {
+        searchInput.style.width = '500px';
+    }
+}
+
+
+handleSearchInputSize(); // 초기 크기 설정
+
+window.addEventListener('resize', () => {
+    // 창 크기에 따라 검색창 너비 조정
+    handleSearchInputSize();
+});
